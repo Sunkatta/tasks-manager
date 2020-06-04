a0e0651f4557949013ea3760e4ee6b165ded31a3
Lusian Manolov <lusianmanolov@outlook.com>
2020-06-04 19:55:07 +0300

    stylized users list

--- src/components/users/users-list/UsersList.jsx
+++ src/components/users/users-list/UsersList.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { getAllUsers, deleteUser, getLoggedUser } from '../../../core/api/users.api';
 import { UserCard } from '../user-card/UserCard';
-
-const currentUser = getLoggedUser();
+import './UsersList.css'
+import { Link } from 'react-router-dom';
 
 export function UsersList() {
     const [users, setUsers] = useState([]);
+    const currentUser = getLoggedUser();
 
     useEffect(() => {
         getAllUsers().then((response) => {
@@ -24,8 +25,21 @@ export function UsersList() {
     }
     
     return (
-        <div className="users-list d-flex">
-            {users.filter(u => currentUser && u.id !== currentUser.id).map((user) => <UserCard user={user} key={user.id} onDelete={onUserDelete}></UserCard>)}
+        <div className="users-list">
+            <>
+            {currentUser.isAdmin &&
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="text-right">
+                        <Link to="/users/create" className="btn btn-primary m-3">Create User</Link>
+                    </div>
+                </div>
+            </div>
+            }
+            </>
+            <div className="row">
+                {users.filter(u => currentUser && u.id !== currentUser.id).map((user) => <UserCard user={user} key={user.id} onDelete={onUserDelete}></UserCard>)}
+            </div>
         </div>
     )
 }
