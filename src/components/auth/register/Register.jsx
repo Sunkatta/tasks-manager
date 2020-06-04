3fd8e9366fd349d19e3a28a0798e46b35285c9e5
Lusian Manolov <lusianmanolov@outlook.com>
2020-06-04 19:48:43 +0300

    reworked register compenent to be functional instead of class component

--- src/components/auth/register/Register.jsx
+++ src/components/auth/register/Register.jsx
@@ -1,72 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Register.css';
 import { register } from '../../../core/api/users.api';
 import { Redirect, Link } from 'react-router-dom';
 
-export class Register extends Component {
+export function Register() {
 
-    constructor(props) {
-        super(props);
+    const[user, setUser] = useState({name: '', email: '', password: '', age: ''});
 
-        this.state = {
-            name: '',
-            email: '',
-            password: '',
-            age: '',
-            isRegistered: false
-        }
-    }
-
-    onInputChange = (event) => {
+    const onInputChange = (event) => {
         event.persist();
 
-        this.setState({
+        setUser((previousState) => ({
+            ...previousState,
             [event.target.name]: event.target.value
-        });
+        }));
     }
 
-    onFormSubmit = (event) => {
+    const onFormSubmit = (event) => {
         event.preventDefault();
 
-        const { isRegistered, ...user } = this.state;
-
         register(user).then(() => {
-            this.setState({
+            setUser({
+                ...user,
                 isRegistered: true
             });
         }).catch((error) => {
-            this.setState({ errorMessage: error.message });
+            setUser({ errorMessage: error.message });
         });
     }
 
-    render() {
-        return (
-            <>
-            { this.state.isRegistered && <Redirect to="/login"></Redirect>}
-            <div className="register-wrapper">
-                <form className="register-form" onSubmit={this.onFormSubmit}>
-                    {this.state.errorMessage && <span className="text-danger">{this.state.errorMessage}</span>}
-                    <div className="form-group">
-                        <label htmlFor="name">Name: </label>
-                        <input type="text" name="name" id="name" className="form-control" onChange={this.onInputChange} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="age">Age: </label>
-                        <input type="number" name="age" id="age" className="form-control" onChange={this.onInputChange} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email">Email: </label>
-                        <input type="email" name="email" id="email" className="form-control" onChange={this.onInputChange} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password: </label>
-                        <input type="password" name="password" id="password" className="form-control" onChange={this.onInputChange} />
-                    </div>
-                    <button className="btn btn-primary">Register</button>
-                    <Link to="/login">Go to Login</Link>
-                </form>
-            </div>
-            </>
-        );
-    }
+    return (
+        <>
+        { user.isRegistered && <Redirect to="/login"></Redirect>}
+        <div className="register-wrapper">
+            <form className="register-form" onSubmit={onFormSubmit}>
+                {user.errorMessage && <span className="text-danger">{user.errorMessage}</span>}
+                <div className="form-group">
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" name="name" id="name" className="form-control" onChange={onInputChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="age">Age: </label>
+                    <input type="number" name="age" id="age" className="form-control" onChange={onInputChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" name="email" id="email" className="form-control" onChange={onInputChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" name="password" id="password" className="form-control" onChange={onInputChange} />
+                </div>
+                <button className="btn btn-primary">Register</button>
+                <Link to="/login">Go to Login</Link>
+            </form>
+        </div>
+        </>
+    );
 }
