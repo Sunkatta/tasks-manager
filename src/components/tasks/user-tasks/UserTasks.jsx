12183794e60f1931f2dfd106f8ada838609c2db7
Lusian Manolov <lusianmanolov@outlook.com>
2020-06-06 18:36:30 +0300

    extended UserTasks component

--- src/components/tasks/user-tasks/UserTasks.jsx
+++ src/components/tasks/user-tasks/UserTasks.jsx
@@ -1,31 +1,65 @@
 import React from 'react';
 import { useState } from 'react';
-import { getUserTasks } from '../../../core/api/tasks.api';
+import { getUserTasks, taskStatus, deleteTask } from '../../../core/api/tasks.api';
 import { useEffect } from 'react';
 import { TaskCard } from '../task-card/TaskCard';
 import { Link } from 'react-router-dom';
 
-export function UserTasks(props) {
+export function UserTasks() {
     const [userTasks, setUserTasks] = useState([]);
     
     useEffect(() => {
-        const searchParams = props.location.search.split('=')[1];
-        getUserTasks(searchParams).then((tasks) => {
+        getUserTasks().then((tasks) => {
             setUserTasks(tasks);
         });
-    }, [props.location.search]);
+    }, []);
+
+    const onDelete = (id) => {
+        deleteTask(id).then(() => {
+            setUserTasks((prevState) => {
+                return prevState.filter(task => task.id !== id);
+            });
+
+            if (localStorage.getItem('userTasks')) {
+                localStorage.removeItem('userTasks');
+            }
+
+            getUserTasks().then((userTasks) => {
+                localStorage.setItem('userTasks', JSON.stringify(userTasks));
+            }, (error) => {
+                console.log(error);
+            });
+        });
+    }
     
     return (
         <div className="user-tasks-wrapper">
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="text-right">
-                        <Link to="/tasks" className="btn btn-primary m-3">All Tasks</Link>
-                        <Link to="/tasks/create" className="btn btn-primary m-3">Create task</Link>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="text-right">
+                            <Link to="/tasks" className="btn btn-primary m-3">All Tasks</Link>
+                            <Link to="/tasks/create" className="btn btn-primary m-3">Create task</Link>
+                        </div>
                     </div>
                 </div>
             </div>
-            { userTasks.map(task => <TaskCard task={task} key={task.id}></TaskCard>) }
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3">
+                        <h3>To Do</h3>
+                        { userTasks.map((task) => (task.status === taskStatus.ToDo && <TaskCard task={task} key={task.id} onDeleteClick={onDelete}></TaskCard>))}
+                    </div>
+                    <div className="col-md-3">
+                        <h3>In Progress</h3>
+                        { userTasks.map((task) => (task.status === taskStatus.InProgress && <TaskCard task={task} key={task.id} onDeleteClick={onDelete}></TaskCard>))}
+                    </div>
+                    <div className="col-md-3">
+                        <h3>Done</h3>
+                        { userTasks.map((task) => (task.status === taskStatus.Done && <TaskCard task={task} key={task.id} onDeleteClick={onDelete}></TaskCard>))}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
